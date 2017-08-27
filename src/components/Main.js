c5b5b88272277ea86fa8e686ca7272f75c40b3bf
Kingsley008 <a837350320@qq.com>
2017-08-27 14:34:59 +0800

    init

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,19 @@
 require('normalize.css/normalize.css');
 require('styles/App.scss');
+var imgeData = require('../data/imgesdata.json');
+// OK ! console.log(imgeData); json loader 正常使用
+
+// 使用自执行函数 得到图片路径
+imgeData = (function getImagsUrl(imgeData) {
+  for (var i = 0; i < imgeData.length; i++) {
+    var singleImg = {};
+    singleImg.fileURL = require('../images/' + imgeData[i].fileName);
+    imgeData[i] = singleImg;
+  }
+  return imgeData;
+})(imgeData)
+
+console.log(imgeData);
 
 import React from 'react';
 
@@ -8,15 +22,18 @@ let yeomanImage = require('../images/yeoman.png');
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
